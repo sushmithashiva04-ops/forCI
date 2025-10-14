5dee4d248c14e6331200155690b3d5b6820c31bf
meena <meenananditha.s.2023.cse@ritchennai.edu.in>
2025-10-14 19:05:07 +0530

    intial push

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # allow frontend to access API
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello from backend!"})
+
+@app.route('/data')
+def data():
+    return jsonify({"data": [1, 2, 3, 4, 5]})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
